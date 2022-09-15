ad25e20c8b43ef85d9cb413e4aab46c6576fe4a4
Tiago César <55418034+cesartiago@users.noreply.github.com>
2022-09-14 22:34:51 -0300

    Update valida_cadastro.php

--- aluno/valida_cadastro.php
+++ aluno/valida_cadastro.php
@@ -3,12 +3,12 @@
     include_once("conexao.php");
 
     if(isset($_POST['novocadastro'])){
-        $nome = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+        $nome = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS); //'name' aqui referencia o id da tag <input type="text" name="name">
         $cpf = filter_input(INPUT_POST, 'cpf', FILTER_SANITIZE_NUMBER_INT);
 
     }
 
-    $result_usuario = "INSERT INTO usuario(nome, cpf) VALUES ('$nome', '$cpf')";
+    $result_usuario = "INSERT INTO usuario(nome, cpf) VALUES ('$nome', '$cpf')"; //INSERT INTO aqui, é a parte que faz INSERT no banco "usuario" que tem as colunas "nome" e "cpf"
     $resultado_usuario = mysqli_query($conn, $result_usuario);
 
 ?>
